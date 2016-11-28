6bebb73e491d8275497a868c1a8ec36d90bc270a
kriox26 <matias.pan26@gmail.com>
2016-11-28 15:10:53 -0300

    Refactor a few tests and added a new one
    The new test checks validity of the output program generated by the
    compiler

--- taqcompiler_test.go
+++ taqcompiler_test.go
@@ -1,6 +1,8 @@
 package taqcompiler
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -21,12 +23,10 @@ end
 
 func TestNewCompilerFromString(t *testing.T) {
 	comp := NewCompilerFromString(correctProgram)
-	if comp != nil {
-		for k, v := range comp.OutputProgram {
-			if v != "0000000000000000" {
-				t.Errorf("compiler.OutputProgram should be inicialize with '0000000000000000' and instead has %s in the index %v", v, k)
-			}
-		}
+	err := testOutputProgramInit(comp)
+	if err != nil {
+		t.Error(err.Error())
+
 	}
 }
 
@@ -36,13 +36,22 @@ func TestNewCompilerFromFile(t *testing.T) {
 		t.Errorf("Failed when opening the test file. Error: %s", err.Error())
 	}
 	comp := NewCompilerFromFile(f)
+	err = testOutputProgramInit(comp)
+	if err != nil {
+		t.Error(err.Error())
+
+	}
+}
+
+func testOutputProgramInit(comp *Compiler) error {
 	if comp != nil {
 		for k, v := range comp.OutputProgram {
 			if v != "0000000000000000" {
-				t.Errorf("compiler.OutputProgram should be inicialize with '0000000000000000' and instead has %s in the index %v", v, k)
+				return fmt.Errorf("compiler.OutputProgram should be inicialize with '0000000000000000' and instead has %s in the index %v", v, k)
 			}
 		}
 	}
+	return nil
 }
 
 func BenchmarkCompile(b *testing.B) {
@@ -103,4 +112,40 @@ func TestCompile(t *testing.T) {
 	if !strings.HasPrefix(comp.CompilationErrors[0], errLabelAlreadyDefined.Error()) {
 		t.Errorf("The error should be errLabelAlreadyDefined. But instead: %s\n", comp.CompilationErrors[0])
 	}
+}
+
+func TestOutputProgram(t *testing.T) {
+	comp := NewCompilerFromString(correctProgram)
+	comp.Compile()
+	f, err := os.Open("testfiles/outputProgramCorrect")
+	defer f.Close()
+	if err != nil {
+		t.Errorf("Failed when opening the test file. Error: %s", err.Error())
+	}
+	f2, err := os.Create("testfiles/testingFile")
+	defer f2.Close()
+	if err != nil {
+		t.Errorf("Failed when opening the test file. Error: %s", err.Error())
+	}
+
+	w := bufio.NewWriter(f2)
+	for _, v := range comp.OutputProgram {
+		w.WriteString(v + "\n")
+	}
+
+	var outputP []string
+	scn := bufio.NewScanner(f)
+	for scn.Scan() {
+		line := scn.Text()
+		// remove all tabs and new lines chars
+		line = strings.Replace(strings.Replace(line, "\t", "", -1), "\n", "", -1)
+		outputP = append(outputP, line)
+	}
+
+	for k, v := range comp.OutputProgram {
+		if v != outputP[k] {
+			t.Errorf("Wrong compilation. Line %v is %v and it should be %v", k, v, outputP[k])
+		}
+	}
+
 }
